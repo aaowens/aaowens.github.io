b682b6a6f5c5a025bc6f0d9e3438957f2f530c26
Andrew Owens <owensaa@gmail.com>
2020-01-13 20:38:00 -0500

    misc

--- _misc/ExampleProject/code.jl
+++
@@ -1,9 +0,0 @@
-using Pkg
-pkg"activate ."
-pkg"instantiate"
-using DataFrames
-using GLM
-x1 = rand(100); x2 = rand(100); eps = rand(100)
-y = 2x1 .+ 3x2 .+ eps
-df = DataFrame(y = y, x1 = x1, x2 = x2, eps = eps)
-ols = lm(@formula(y ~ x1 + x2), df)
